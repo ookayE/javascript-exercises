0451b260546c41c4ceb78433973485d29d230b5d
ookayE <smith.evan93@gmail.com>
2023-08-16 23:12:05 -0400

    Continued work on JS Exercises for Odin Project. Currently on Fibonacci function.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = function(string) {
+    const formatString = string.toLowerCase().replace(/[^a-z0-9]/g, "")
+    return formatString.split('').reverse().join('') == formatString
+}
 
-};
+palindromes("making s%$ure this join^%$t here w&*^orks, cuh")
+
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    const n = parseInt(input, 10); // Convert the input string to a number
+    const sequence = [0, 1]
+    
+    if (isNaN(n) || n < 0) {
+        return "OOPS"
+    }
 
-};
+    for (let i = 1; i<= n; i++) {
+        const nextNumber = sequence[sequence.length - 1] + sequence[sequence.length-2]
+        sequence.push(nextNumber)
+    }
+    return sequence[sequence.length-2]
+  }
 
 // Do not edit below this line
 module.exports = fibonacci;
